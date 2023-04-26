46b68a7562b8d4614e33c6b6dd93095cec3eec74
meirlustig <meirlustig1983@gmail.com>
2023-04-25 19:48:19 -0700

    SWL-001 First commit

---
+++ src/main/java/com/ml/springwithlombok/SpringWithLombokApplication.java
@@ -0,0 +1,13 @@
+package com.ml.springwithlombok;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringWithLombokApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringWithLombokApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ml/springwithlombok/SpringWithLombokApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ml.springwithlombok;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringWithLombokApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
