6f77d5fc9186dd7060b83f950897cd444502c1d5
meirlustig <meirlustig1983@gmail.com>
2023-04-27 13:26:34 -0700

    SWL-016 Use @Cleanup for local resources

--- src/main/java/com/ml/springwithlombok/config/InitDataConfig.java
+++ src/main/java/com/ml/springwithlombok/config/InitDataConfig.java
@@ -6,8 +6,9 @@ import com.ml.springwithlombok.dao.EmployeeImage;
 import com.ml.springwithlombok.repositories.AddressRepository;
 import com.ml.springwithlombok.repositories.EmployeeImageRepository;
 import com.ml.springwithlombok.repositories.EmployeeRepository;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import lombok.Cleanup;
+import lombok.extern.slf4j.Slf4j;
+import lombok.val;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -17,10 +18,9 @@ import org.springframework.util.StreamUtils;
 import java.io.IOException;
 import java.io.InputStream;
 
+@Slf4j
 @Configuration
 public class InitDataConfig {
-
-    public static final Logger LOGGER = LoggerFactory.getLogger(InitDataConfig.class);
 
     @Bean
     public CommandLineRunner initData(EmployeeRepository employeeRepository,
@@ -28,8 +28,8 @@ public class InitDataConfig {
                                       EmployeeImageRepository employeeImageRepository) {
         return args -> {
 
-            final Employee.EmployeeBuilder employeeBuilder = Employee.builder();
-            Employee theodoreEmployee = employeeBuilder
+            val employeeBuilder = Employee.builder();
+            val theodoreEmployee = employeeBuilder
                     .firstName("Theodore")
                     .lastName("Roosevelt")
                     .middleName("Cleveland")
@@ -37,7 +37,7 @@ public class InitDataConfig {
                     .email("tcr@gmail.com")
                     .build();
 
-            Employee franklinEmployee = employeeBuilder
+            val franklinEmployee = employeeBuilder
                     .firstName("Franklin")
                     .lastName("Benjamin")
                     .middleName("Paul")
@@ -48,9 +48,9 @@ public class InitDataConfig {
             employeeRepository.save(theodoreEmployee);
             employeeRepository.save(franklinEmployee);
 
-            final Address.AddressBuilder addressBuilder = Address.builder();
+            val addressBuilder = Address.builder();
 
-            Address franklinAddress = addressBuilder
+            val franklinAddress = addressBuilder
                     .employee(franklinEmployee)
                     .addressType("home")
                     .addressLine1("618033 Milk Street")
@@ -59,7 +59,7 @@ public class InitDataConfig {
                     .zipCode("02109")
                     .build();
 
-            Address theodoreAddress = addressBuilder
+            val theodoreAddress = addressBuilder
                     .employee(theodoreEmployee)
                     .addressType("home")
                     .addressLine1("28 East 20th Street")
@@ -72,17 +72,17 @@ public class InitDataConfig {
             addressRepository.save(franklinAddress);
             addressRepository.save(theodoreAddress);
 
-            final EmployeeImage.EmployeeImageBuilder employeeImageBuilder = EmployeeImage.builder();
+            val employeeImageBuilder = EmployeeImage.builder();
 
-            byte[] franklinImg = retrieveStockImage("benjamin_franklin.png");
-            byte[] rooseveltImg = retrieveStockImage("theo_roosevelt.png");
+            val franklinImg = retrieveStockImage("benjamin_franklin.png");
+            val rooseveltImg = retrieveStockImage("theo_roosevelt.png");
 
-            EmployeeImage theodoreImage = employeeImageBuilder
+            val theodoreImage = employeeImageBuilder
                     .employee(theodoreEmployee)
                     .image(rooseveltImg)
                     .build();
 
-            EmployeeImage franklinImage = employeeImageBuilder
+            val franklinImage = employeeImageBuilder
                     .employee(franklinEmployee)
                     .image(franklinImg)
                     .build();
@@ -93,18 +93,11 @@ public class InitDataConfig {
     }
 
     private byte[] retrieveStockImage(String imageName) throws IOException {
-        LOGGER.info("LombokDemoApplication.retrieveStockImage() - attempting to retrieve the image");
-        InputStream in = null;
-        try {
-            ClassPathResource classPathResource = new ClassPathResource(imageName);
-            in = classPathResource.getInputStream();
-            byte[] imageBytes = StreamUtils.copyToByteArray(in);
-            LOGGER.info("LombokDemoApplication.retrieveStockImage() - byte[] length = " + imageBytes.length);
-            return imageBytes;
-        } finally {
-            if (in != null) {
-                in.close();
-            }
-        }
+        log.info("LombokDemoApplication.retrieveStockImage() - attempting to retrieve the image");
+        ClassPathResource classPathResource = new ClassPathResource(imageName);
+        @Cleanup InputStream in = classPathResource.getInputStream();
+        byte[] imageBytes = StreamUtils.copyToByteArray(in);
+        log.info("LombokDemoApplication.retrieveStockImage() - byte[] length = " + imageBytes.length);
+        return imageBytes;
     }
 }
